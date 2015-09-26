8f16e93b33320fe843fecbb521bb621791c8f63e
MrGuill666 <gergelyszaz@gmail.com>
2015-09-26 04:18:52 +0200

    working

---
+++ src/main/ItemListService.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ * Created by mad on 2015. 09. 24..
+ */
+/*
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class ItemListService {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}*/
